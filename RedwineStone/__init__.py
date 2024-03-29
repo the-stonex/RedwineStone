4f13f6b2b42ad22588e91533f13c59ca21dc5dfd
The-stonex <147587252+the-stonex@users.noreply.github.com>
2024-03-29 13:38:48 +0530

    Create __init__.py

---
+++ RedwineStone/__init__.py
@@ -0,0 +1,26 @@
+from RedwineStone.core.bot import Sang
+from RedwineStone.core.dir import dirr
+from RedwineStone.core.git import git
+from RedwineStone.core.userbot import Userbot
+from RedwineStone.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Sang()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
