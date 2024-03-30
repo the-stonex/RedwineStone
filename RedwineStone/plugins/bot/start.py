9edaff8ce90ad59f7d1632e9b197b7da69c5f0f2
The-stonex <147587252+the-stonex@users.noreply.github.com>
2024-03-31 03:02:25 +0530

    Update start.py

--- RedwineStone/plugins/bot/start.py
+++ RedwineStone/plugins/bot/start.py
@@ -84,7 +84,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await app.message.send_photo(
+        await app.send_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
