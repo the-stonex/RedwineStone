d1961f03ddd404478cbd264a93f4057537273afb
The-stonex <147587252+the-stonex@users.noreply.github.com>
2024-03-31 05:11:21 +0530

    Update call.py

--- RedwineStone/core/call.py
+++ RedwineStone/core/call.py
@@ -1,3 +1,6 @@
+from RedwineStone.utils.database import AssistantErr
+
+
 import asyncio
 import os
 from datetime import datetime, timedelta
@@ -31,7 +34,7 @@ from RedwineStone.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from RedwineStone.utils.database import AssistantErr
+from RedwineStone.utils.exceptions import AssistantErr
 from RedwineStone.utils.formatters import check_duration, seconds_to_min, speed_converter
 from RedwineStone.utils.inline.play import stream_markup
 from RedwineStone.utils.stream.autoclear import auto_clean
@@ -51,7 +54,7 @@ async def _clear_(chat_id):
 class Call(PyTgCalls):
     def __init__(self):
         self.userbot1 = Client(
-            name="StoneAss1",
+            name="AnonXAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
@@ -61,7 +64,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot2 = Client(
-            name="StoneAss2",
+            name="AnonXAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
@@ -71,7 +74,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot3 = Client(
-            name="StoneAss3",
+            name="AnonXAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
@@ -81,7 +84,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot4 = Client(
-            name="StoneAss4",
+            name="AnonXAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
@@ -91,7 +94,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot5 = Client(
-            name="StoneAss5",
+            name="AnonXAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -596,6 +599,5 @@ class Call(PyTgCalls):
             if not isinstance(update, StreamAudioEnded):
                 return
             await self.change_stream(client, update.chat_id)
-
 
 Sang = Call()
